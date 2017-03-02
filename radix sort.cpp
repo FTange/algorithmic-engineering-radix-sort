03b636722c02623247e940645e2640f2c6d7ca88
Frederik Tange <Frederiktange@gmail.com>
2017-03-02 11:54:54 +0100

    added radix sort using counting sort with idea from article

--- radix sort.cpp
+++ radix sort.cpp
@@ -6,8 +6,8 @@ int const bitsSortedOn = 3;
 
 // optimized standard radix sort without copy back
 int radixSortWithoutCopyBack(int *arr, int size, int bitsSortedOn) {
-	int const k = (1 << bitsSortedOn)-1;
-	int const buckets = k+1;
+	int const buckets = 1 << bitsSortedOn;
+	int const k = buckets-1;
 	int freq[buckets];
 	for (int i=0; i<buckets; i++) freq[i] = 0;
 	int *tmp = new int[size];
@@ -49,8 +49,8 @@ int radixSortWithoutCopyBack(int *arr, int size, int bitsSortedOn) {
 
 
 int radixSort(int *arr, int size, int bitsSortedOn) {
-	int const k = (1 << bitsSortedOn)-1;
-	int const buckets = k+1;
+	int const buckets = 1 << bitsSortedOn;
+	int const k = buckets-1;
 	int freq[buckets]; 
 	for (int i=0; i<buckets; i++) freq[i] = 0;
 	int *tmp = new int[size];
@@ -75,6 +75,43 @@ int radixSort(int *arr, int size, int bitsSortedOn) {
 	return 0;
 }
 
+/*
+ * Idea from algorithm 1 in article
+ * Allocate buckets for each possible value for the sorted in bits
+ * Uses much more memory when sorting on more bits
+ */
+int radixSortWoCountingFreq(int *arr, int size, int bitsSortedOn) {
+	int const buckets = 1 << bitsSortedOn;
+	int const k = buckets-1;
+	int freq[buckets];
+	for (int i=0; i<buckets; i++) freq[i] = 0;
+	// can't use new since buckets and size aren't known and compile time
+	int *tmp = (int *) malloc(sizeof(int) * size * buckets);
+	int shift = 0;
+
+	while (shift < 32) {
+		for (int i=0; i < size; i++) {
+			int bucket = (arr[i] >> shift) & k;
+			tmp[freq[bucket] + size * bucket] = arr[i];
+			freq[bucket]++;
+		}
+		// Go through each bucket and copy all its content back to arr
+		int elem = size-1;
+		for (int bucket=buckets-1; bucket>=0; bucket--) {
+			while (freq[bucket] != 0) {
+				arr[elem--] = tmp[bucket*size + --freq[bucket]];
+			}
+		}
+		for (int i=0; i<buckets; i++) { // set frequencies to 0
+			freq[i] = 0; }
+		shift += bitsSortedOn;
+	}
+
+	free(tmp);
+
+	return 0;
+}
+
 bool isSorted(int *arr, int size) {
 	for (int i=0, end = size-1; i<end; i++) {
 		if (arr[i] > arr[i+1]) {
@@ -89,7 +126,7 @@ void testBitsSortedOn(int N, int reps) {
 	for (int i = 0; i<N; i++) {
 		array[i] = rand();
 	}
-	int times[17] = {0};
+	int times[17] = {0}; // larger than 16 doesn't make really make sense
 	for (int k=0; k<=reps;k++) {
 		for (int i=1; i<=16;i++) {
 			for (int j = 0; j<N; j++) {
@@ -106,4 +143,4 @@ void testBitsSortedOn(int N, int reps) {
 	delete[] array;
 }
 
-int main() {}
+int main() { }
