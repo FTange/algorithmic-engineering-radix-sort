4fce65e2e77706db4aa4c786c0f4d6b00e9672ed
Frederik Tange <Frederiktange@gmail.com>
2017-03-03 09:17:53 +0100

    added vers with frequency matrix

--- radix sort.cpp
+++ radix sort.cpp
@@ -16,8 +16,7 @@ void printArray(int *arr, int n) {
 	}
 }
 
-// optimized standard radix sort without copy back
-int radixSortWithoutCopyBack(int *arr, int size, int bitsSortedOn) {
+int radixSort(int *arr, int size, int bitsSortedOn) {
 	int const buckets = 1 << bitsSortedOn;
 	int const k = buckets-1;
 	int freq[buckets];
@@ -28,7 +27,67 @@ int radixSortWithoutCopyBack(int *arr, int size, int bitsSortedOn) {
 	while (shift < 32) {
 		for (int i=0; i<size; i++) { // count frequencies
 			freq[(arr[i] >> shift) & k]++; }
-		for (int i=1; i<buckets; i++) { // sum frequencies 
+		for (int i=1; i<buckets; i++) { // sum frequencies
+			freq[i] += freq[i-1]; }
+		for (int i=size-1; i >= 0; i--) { // move nodes correct loc in tmp array
+			tmp[--freq[(arr[i] >> shift) & k]] = arr[i]; }
+		for (int i=0; i<buckets; i++) { // set frequencies to 0
+			freq[i] = 0; }
+		for (int i=0; i<size; i++) { // copy from tmp back to arr
+			arr[i] = tmp[i]; }
+		shift += bitsSortedOn;
+	}
+
+	delete[] tmp;
+
+	return 0;
+}
+
+//
+int radixSortFreqMatrix(int *arr, int size, int bitsSortedOn) {
+	int const buckets = 1 << bitsSortedOn;
+	int const k = buckets-1;
+	int const passes = 32 / bitsSortedOn + 1;
+	// better to store on heap with optimized zero assignment?
+	int freq[passes][buckets];
+	for (int i=0; i<passes; i++)
+		for (int j=0; j<buckets; j++)
+			freq[i][j] = 0;
+	int *tmp = new int[size];
+	int shift = 0, iteration = 0;
+
+	while (shift < 32) {
+		cout << iteration << endl;
+		for (int i=0; i<size; i++) { // count frequencies
+			freq[iteration][(arr[i] >> shift) & k]++; }
+		for (int i=1; i<buckets; i++) { // sum frequencies
+			freq[iteration][i] += freq[iteration][i-1]; }
+		for (int i=size-1; i >= 0; i--) { // move nodes correct loc in tmp array
+			tmp[--freq[iteration][(arr[i] >> shift) & k]] = arr[i]; }
+		for (int i=0; i<size; i++) { // copy from tmp back to arr
+			arr[i] = tmp[i]; }
+		shift += bitsSortedOn;
+		iteration++;
+	}
+
+	delete[] tmp;
+
+	return 0;
+}
+
+// optimized standard radix sort without copy back
+int radixSortWoCopyBack(int *arr, int size, int bitsSortedOn) {
+	int const buckets = 1 << bitsSortedOn;
+	int const k = buckets-1;
+	int freq[buckets];
+	for (int i=0; i<buckets; i++) freq[i] = 0;
+	int *tmp = new int[size];
+	int shift = 0;
+
+	while (shift < 32) {
+		for (int i=0; i<size; i++) { // count frequencies
+			freq[(arr[i] >> shift) & k]++; }
+		for (int i=1; i<buckets; i++) { // sum frequencies
 			freq[i] += freq[i-1]; }
 		for (int i=size-1; i >= 0; i--) { // move nodes correct loc in tmp array
 			tmp[--freq[(arr[i] >> shift) & k]] = arr[i]; }
@@ -59,31 +118,45 @@ int radixSortWithoutCopyBack(int *arr, int size, int bitsSortedOn) {
 	return 0;
 }
 
-
-int radixSort(int *arr, int size, int bitsSortedOn) {
+int radixSortWoCopyBackFreqMatrix(int *arr, int size, int bitsSortedOn) {
 	int const buckets = 1 << bitsSortedOn;
 	int const k = buckets-1;
-	int freq[buckets]; 
-	for (int i=0; i<buckets; i++) freq[i] = 0;
+	int const passes = 32 / bitsSortedOn + 1;
+	int freq[passes][buckets];
+	for (int i=0; i<passes; i++)
+		for (int j=0; j<buckets; j++)
+			freq[i][j] = 0;
 	int *tmp = new int[size];
-	int shift = 0;
+	int shift = 0, iteration = 0;
 
 	while (shift < 32) {
 		for (int i=0; i<size; i++) { // count frequencies
-			freq[(arr[i] >> shift) & k]++; }
-		for (int i=1; i<buckets; i++) { // sum frequencies 
-			freq[i] += freq[i-1]; }
+			freq[iteration][(arr[i] >> shift) & k]++; }
+		for (int i=1; i<buckets; i++) { // sum frequencies
+			freq[iteration][i] += freq[iteration][i-1]; }
 		for (int i=size-1; i >= 0; i--) { // move nodes correct loc in tmp array
-			tmp[--freq[(arr[i] >> shift) & k]] = arr[i]; }
-		for (int i=0; i<buckets; i++) { // set frequencies to 0
-			freq[i] = 0; }
-		for (int i=0; i<size; i++) { // copy from tmp back to arr
-			arr[i] = tmp[i]; }
-		shift += bitsSortedOn;
-	}
+			tmp[--freq[iteration][(arr[i] >> shift) & k]] = arr[i]; }
 
+		iteration++;
+		shift += bitsSortedOn;
+		if (!(shift < 32)) { break; }
+
+		for (int i=0; i<size; i++) { // count frequencies
+			freq[iteration][(tmp[i] >> shift) & k]++; }
+		for (int i=1; i<buckets; i++) { // sum frequencies
+			freq[iteration][i] += freq[iteration][i-1]; }
+		for (int i=size-1; i >= 0; i--) { // move nodes to correct loc in arr
+			arr[--freq[iteration][(tmp[i] >> shift) & k]] = tmp[i]; }
+		shift += bitsSortedOn;
+		iteration++;
+	}
+	if (32 % bitsSortedOn != 0) {
+		for (int i=0; i<size; i++) {
+			arr[i] = tmp[i];
+		}
+	}
 	delete[] tmp;
-	
+
 	return 0;
 }
 
@@ -100,7 +173,6 @@ int radixSortWoCountingFreq(int *arr, int size, int bitsSortedOn) {
 	// can't use new since buckets and size aren't known and compile time
 	int *tmp = (int *) malloc(sizeof(int) * size * buckets);
 	int shift = 0;
-
 	while (shift < 32) {
 		for (int i=0; i < size; i++) {
 			int bucket = (arr[i] >> shift) & k;
@@ -139,13 +211,13 @@ void testBitsSortedOn(int N, int reps) {
 		array[i] = rand();
 	}
 	int times[17] = {0}; // larger than 16 doesn't make really make sense
-	for (int k=0; k<=reps;k++) {
+	for (int k=0; k<reps; k++) {
 		for (int i=1; i<=16;i++) {
 			for (int j = 0; j<N; j++) {
 				array[j] = rand();
 			}
 			clock_t t = clock();
-			radixSort(array, N, i);
+			radixSortWoCountingFreq(array, N, i);
 			times[i] += clock() - t;
 		}
 	}
@@ -155,4 +227,5 @@ void testBitsSortedOn(int N, int reps) {
 	delete[] array;
 }
 
-int main() { }
+int main() {
+}
